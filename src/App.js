7e4d4cd143ef73b6dc79824a97a119ea0f69f2c8
mintyjones <gordon@zeedesigns.com.au>
2021-06-18 23:14:46 +1000

    fixed typo in CSS and added Header to App comp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div >
-          The basic empty template
+          <Header />
     </div>
   )
 }
--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ const Title = () => {
     return (
         <div>
             <h1 className="font-PoiretOne">Gordon Campbell</h1>
-            <h2 className="font-RacingSansOne">&lt; Full Stack Dev ></h2>
+            <h2 className="font-RacingSansOne">&lt; Full Stack Dev &gt;</h2>
         </div>
     )
 }
