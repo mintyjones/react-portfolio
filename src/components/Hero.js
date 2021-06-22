27403cadbd7a02f8e3441e1aaf8b4a62c96ff887
mintyjones <gordon@zeedesigns.com.au>
2021-06-22 16:18:19 +1000

    quick change to check continued deployment via github

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
         <div className="bg-black p-8 w-screen">
             <p className="flex flex-col items-center">
                 <p className="font-PoiretOne text-3xl text-white">Nothing is impossible...</p>
-                <p className="font-RacingSansOne text-2xl text-primaryColLite pt-4">&#123; ANYTHING IS POSSIBLE &#125;</p>
+                <p className="font-RacingSansOne text-2xl text-primaryColLite pt-4">&#123; ANYTHING IS POSSIBLE! &#125;</p>
             </p>
         </div>
     )
