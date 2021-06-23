54c0dbc26b0c508cd02ddcd18f1b79fb8fcefb6e
mintyjones <gordon@zeedesigns.com.au>
2021-06-23 23:58:08 +1000

    breakpoint adjust for ipad portrait

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,8 +12,8 @@ const Hero = ({tablet}) => {
     return (
         <div className={`bg-black p-7 w-screen tablets:h-[336px] tablets:flex tablets:items-center tablets:justify-end extraLG:justify-center tablets:w-auto ${skewStyle}`}>
             <div className="flex flex-col items-center tablets:items-start">
-                <p className="font-PoiretOne text-3xl tablets:text-4xl desktop:text-6xl text-white">Nothing is impossible...</p>
-                <p className="font-RacingSansOne text-2xl tablets:text-4xl desktop:text-6xl text-primaryColLite pt-4">&#123; ANYTHING IS POSSIBLE &#125;</p>
+                <p className="font-PoiretOne text-3xl tablets:text-[2.35rem] laptops:text-5xl desktop:text-[4.6rem] text-white">Nothing is impossible...</p>
+                <p className="font-RacingSansOne text-2xl tablets:text-[1.95rem] laptops:text-[2.5rem] desktop:text-6xl text-primaryColLite pt-4">&#123; ANYTHING IS POSSIBLE &#125;</p>
             </div>
         </div>
     )
