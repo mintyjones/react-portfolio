029e9d9404c2e72eb0d7836724788e2ab4efec56
mintyjones <gordon@zeedesigns.com.au>
2021-06-30 15:57:09 +1000

    changed Title to flex-col upto laptops breakpoint

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ import { Link } from 'react-scroll'
 
 const Title = () => {
     return (
-        <div className="flex landscape:flex-col tablets: flex-row">
+        <div className="flex landscape:flex-col flex-col laptops:flex-row">
             <span className="font-PoiretOne text-xl">Gordon Campbell&nbsp;</span>
             <span className="font-RacingSansOne text-xl">&lt; FULL_STACK_DEV &gt;</span>
         </div>
